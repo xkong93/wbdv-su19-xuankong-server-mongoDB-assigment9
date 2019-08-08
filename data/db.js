7d32a1f22544c2b5f297b884952ab281210cde04
XuanKong <k.xuan29@gmail.com>
2019-08-07 17:37:37 -0700

    change url

--- data/db.js
+++ data/db.js
@@ -1,7 +1,12 @@
 module.exports = function () {
     const mongoose = require('mongoose');
-    // mongoose.connect('mongodb://localhost:27017/whiteboard-su19', {useNewUrlParser: true});
-    var username = process.env.MY_MONGODB_USERNAME;
-    var password = process.env.MY_MONGODB_PASSWORD;
-    mongoose.connect('mongodb://'+username+':'+password+'@ds261077.mlab.com:61077/heroku_h2s9fzj5', {useNewUrlParser: true});
+    var connectionString = 'mongodb://localhost:27017/whiteboard-su19';
+
+    if (process.env.MLAB_USERNAME_WEBDEV) {
+        var username = process.env.MLAB_USERNAME_WEBDEV;
+        var password = process.env.MLAB_USERNAME_WEBDEV;
+        mongoose.connect('mongodb://' + username + ':' + password + '@ds261077.mlab.com:61077/heroku_h2s9fzj5', {useNewUrlParser: true});
+    }else{
+        mongoose.connect(connectionString, {useNewUrlParser: true});
+    }
 }
