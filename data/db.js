83f7b173e31716301fed27673ea3582f5a327252
XuanKong <k.xuan29@gmail.com>
2019-08-07 17:17:15 -0700

    url

--- data/db.js
+++ data/db.js
@@ -1,4 +1,7 @@
 module.exports = function () {
     const mongoose = require('mongoose');
-    mongoose.connect('mongodb://localhost:27017/whiteboard-su19', {useNewUrlParser: true});
+    // mongoose.connect('mongodb://localhost:27017/whiteboard-su19', {useNewUrlParser: true});
+    var username = process.env.MY_MONGODB_USERNAME;
+    var password = process.env.MY_MONGODB_PASSWORD;
+    mongoose.connect('mongodb://'+username+':'+password+'@ds261077.mlab.com:61077/heroku_h2s9fzj5', {useNewUrlParser: true});
 }
