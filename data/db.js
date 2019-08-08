5169f6cb77f649bfb38f486ea58208a36f80464b
XuanKong <k.xuan29@gmail.com>
2019-08-07 17:47:34 -0700

    changes

--- data/db.js
+++ data/db.js
@@ -1,8 +1,11 @@
 module.exports = function () {
     const mongoose = require('mongoose');
 
-    var username = process.env.MLAB_USERNAME_WEBDEV;
-    var password = process.env.MLAB_USERNAME_WEBDEV;
-    mongoose.connect('mongodb://' + username + ':' + password + '@ds261077.mlab.com:61077/heroku_h2s9fzj5', {useNewUrlParser: true});
-}
+    if (process.env.MLAB_USERNAME_WEBDEV) {
+        var username = process.env.MLAB_USERNAME_WEBDEV;
+        var password = process.env.MLAB_PASSWORD_WEBDEV;
+        mongoose.connect('mongodb://' + username + ':' + password + '@ds261077.mlab.com:61077/heroku_h2s9fzj5', {useNewUrlParser: true});
+    }else{
+        mongoose.connect(connectionString, {useNewUrlParser: true});
+    }
 }
